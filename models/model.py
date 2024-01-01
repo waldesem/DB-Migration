4c30a8e6b3a0bd651e1ed764d07143cb9c609ec2
Vladimir Semenenko <wsemenenko@gmail.com>
2024-01-01 20:13:50 +0300

    Update models

--- models/model.py
+++ models/model.py
@@ -4,7 +4,7 @@ from typing import List
 from sqlalchemy import Boolean, create_engine, ForeignKey, String, Text, Date, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
-from models.classes import Conclusions
+from classes import Conclusions
 
 
 engine = create_engine('sqlite:///persons.db', echo=True)
@@ -58,7 +58,7 @@ class Region(Base):
 
     __tablename__ = 'regions'
     
-    id: Mapped[int] = mapped_column(nullable=False, unique=True, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(nullable=True, unique=True, primary_key=True, autoincrement=True)
     region: Mapped[str] = mapped_column(String(255))
     persons : Mapped[List['Person']] = relationship(back_populates='regions')
    
@@ -80,20 +80,20 @@ class Person(Base):
     category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))
     region_id: Mapped[int] = mapped_column(ForeignKey('regions.id'))
     fullname: Mapped[str] = mapped_column(String(255), nullable=False, index=True)
-    previous: Mapped[str] = mapped_column(Text)
+    previous: Mapped[str] = mapped_column(Text, nullable=True)
     birthday: Mapped[datetime] = mapped_column(Date, nullable=False, index=True)
-    birthplace: Mapped[str] = mapped_column(Text)
-    country: Mapped[str] = mapped_column(String(255))
-    ext_country: Mapped[str] = mapped_column(String(255))
-    snils: Mapped[str] = mapped_column(String(11))
-    inn: Mapped[str] = mapped_column(String(12))
-    education: Mapped[str] = mapped_column(Text)
-    marital: Mapped[str] = mapped_column(String(255))
-    addition: Mapped[str] = mapped_column(Text)
-    path: Mapped[str] = mapped_column(Text)
+    birthplace: Mapped[str] = mapped_column(Text, nullable=True)
+    country: Mapped[str] = mapped_column(String(255), nullable=True)
+    ext_country: Mapped[str] = mapped_column(String(255), nullable=True)
+    snils: Mapped[str] = mapped_column(String(11), nullable=True)
+    inn: Mapped[str] = mapped_column(String(12), nullable=True)
+    education: Mapped[str] = mapped_column(Text, nullable=True)
+    marital: Mapped[str] = mapped_column(String(255), nullable=True)
+    addition: Mapped[str] = mapped_column(Text, nullable=True)
+    path: Mapped[str] = mapped_column(Text, nullable=True)
     status_id: Mapped[int] = mapped_column(ForeignKey('statuses.id'))
-    create: Mapped[datetime] = mapped_column(Date, default=default_time)
-    update: Mapped[datetime] = mapped_column(Date, onupdate=default_time)
+    create: Mapped[datetime] = mapped_column(Date, default=default_time, nullable=True)
+    update: Mapped[datetime] = mapped_column(Date, onupdate=default_time, nullable=True)
     staffs: Mapped[List['Staff']] = relationship(
         back_populates='persons', cascade="all, delete, delete-orphan"
         )
@@ -132,8 +132,8 @@ class Staff(Base):
     __tablename__ = 'staffs'
 
     id: Mapped[int] = mapped_column(nullable=False, unique=True, primary_key=True, autoincrement=True)
-    position: Mapped[str] = mapped_column(Text)
-    department: Mapped[str] = mapped_column(Text)
+    position: Mapped[str] = mapped_column(Text, nullable=True)
+    department: Mapped[str] = mapped_column(Text, nullable=True)
     person_id: Mapped[int] = mapped_column(ForeignKey('persons.id'))
     persons: Mapped[List['Person']] = relationship(back_populates='staffs')
 
@@ -144,12 +144,11 @@ class Document(Base):
     __tablename__ = 'documents'
 
     id: Mapped[int] = mapped_column(nullable=False, unique=True, primary_key=True, autoincrement=True)
-
-    view: Mapped[str] = mapped_column(String(255))
-    series: Mapped[str] = mapped_column(String(255))
-    number: Mapped[str] = mapped_column(String(255))
-    agency: Mapped[str] = mapped_column(Text)
-    issue: Mapped[datetime] = mapped_column(Date)
+    view: Mapped[str] = mapped_column(String(255), nullable=True)
+    series: Mapped[str] = mapped_column(String(255), nullable=True)
+    number: Mapped[str] = mapped_column(String(255), nullable=True)
+    agency: Mapped[str] = mapped_column(Text, nullable=True)
+    issue: Mapped[datetime] = mapped_column(Date, nullable=True)
     person_id: Mapped[int] = mapped_column(ForeignKey('persons.id'))
     persons: Mapped[List['Person']] = relationship(back_populates='documents')
 
@@ -160,9 +159,9 @@ class Address(Base):
     __tablename__ = 'addresses'
 
     id: Mapped[int] = mapped_column(nullable=False, unique=True, primary_key=True, autoincrement=True)
-    view: Mapped[str] = mapped_column(String(255))
-    region: Mapped[str] = mapped_column(String(255))
-    address: Mapped[str] = mapped_column(Text)
+    view: Mapped[str] = mapped_column(String(255), nullable=True)
+    region: Mapped[str] = mapped_column(String(255), nullable=True)
+    address: Mapped[str] = mapped_column(Text, nullable=True)
     person_id: Mapped[int] = mapped_column(ForeignKey('persons.id'))
     persons: Mapped[List['Person']] = relationship(back_populates='addresses')
 
@@ -173,8 +172,8 @@ class Contact(Base):
     __tablename__ = 'contacts'
 
     id: Mapped[int] = mapped_column(nullable=False, unique=True, primary_key=True, autoincrement=True)
-    view: Mapped[str] = mapped_column(String(255))
-    contact: Mapped[str] = mapped_column(String(255))
+    view: Mapped[str] = mapped_column(String(255), nullable=True)
+    contact: Mapped[str] = mapped_column(String(255), nullable=True)
     person_id: Mapped[int] = mapped_column(ForeignKey('persons.id'))
     persons: Mapped[List['Person']] = relationship(back_populates='contacts')
 
@@ -185,12 +184,12 @@ class Workplace(Base):
     __tablename__ = 'workplaces'
 
     id: Mapped[int] = mapped_column(nullable=False, unique=True, primary_key=True, autoincrement=True)
-    start_date: Mapped[datetime] = mapped_column(Date)
-    end_date: Mapped[datetime] = mapped_column(Date)
-    workplace: Mapped[str] = mapped_column(String(255))
-    address: Mapped[str] = mapped_column(Text)
-    position: Mapped[str] = mapped_column(Text)
-    reason: Mapped[str] = mapped_column(Text)
+    start_date: Mapped[datetime] = mapped_column(Date, nullable=True)
+    end_date: Mapped[datetime] = mapped_column(Date, nullable=True)
+    workplace: Mapped[str] = mapped_column(String(255), nullable=True)
+    address: Mapped[str] = mapped_column(Text, nullable=True)
+    position: Mapped[str] = mapped_column(Text, nullable=True)
+    reason: Mapped[str] = mapped_column(Text, nullable=True)
     person_id: Mapped[int] = mapped_column(ForeignKey('persons.id'))
     persons: Mapped[List['Person']] = relationship(back_populates='workplaces')
 
@@ -201,11 +200,11 @@ class Affilation(Base):
     __tablename__ = 'affilations'
 
     id: Mapped[int] = mapped_column(nullable=False, unique=True, primary_key=True, autoincrement=True)
-    view: Mapped[str] = mapped_column(String(255))
-    name: Mapped[str] = mapped_column(Text)
-    inn: Mapped[str] = mapped_column(String(255))
-    position: Mapped[str] = mapped_column(Text)
-    deadline: Mapped[datetime] = mapped_column(Date, default=default_time)
+    view: Mapped[str] = mapped_column(String(255), nullable=True)
+    name: Mapped[str] = mapped_column(Text, nullable=True)
+    inn: Mapped[str] = mapped_column(String(255), nullable=True)
+    position: Mapped[str] = mapped_column(Text, nullable=True)
+    deadline: Mapped[datetime] = mapped_column(Date, default=default_time, nullable=True)
     person_id: Mapped[int] = mapped_column(ForeignKey('persons.id'))
     persons: Mapped[List['Person']] = relationship(back_populates='affilations')
 
@@ -216,24 +215,24 @@ class Check(Base):
     __tablename__ = 'checks'
 
     id: Mapped[int] = mapped_column(nullable=False, unique=True, primary_key=True, autoincrement=True)
-    workplace: Mapped[str] = mapped_column(Text)
-    document: Mapped[str] = mapped_column(Text)
-    inn: Mapped[str] = mapped_column(Text)
-    debt: Mapped[str] = mapped_column(Text)
-    bankruptcy: Mapped[str] = mapped_column(Text)
-    bki: Mapped[str] = mapped_column(Text)
-    courts: Mapped[str] = mapped_column(Text)
-    affiliation: Mapped[str] = mapped_column(Text)
-    terrorist: Mapped[str] = mapped_column(Text)
-    mvd: Mapped[str] = mapped_column(Text)
-    internet: Mapped[str] = mapped_column(Text)
-    cronos: Mapped[str] = mapped_column(Text)
-    cros: Mapped[str] = mapped_column(Text)
-    addition: Mapped[str] = mapped_column(Text)
-    pfo: Mapped[bool] = mapped_column(Boolean)
-    comments: Mapped[str] = mapped_column(Text)
+    workplace: Mapped[str] = mapped_column(Text, nullable=True)
+    document: Mapped[str] = mapped_column(Text, nullable=True)
+    inn: Mapped[str] = mapped_column(Text, nullable=True)
+    debt: Mapped[str] = mapped_column(Text, nullable=True)
+    bankruptcy: Mapped[str] = mapped_column(Text, nullable=True)
+    bki: Mapped[str] = mapped_column(Text, nullable=True)
+    courts: Mapped[str] = mapped_column(Text, nullable=True)
+    affiliation: Mapped[str] = mapped_column(Text, nullable=True)
+    terrorist: Mapped[str] = mapped_column(Text, nullable=True)
+    mvd: Mapped[str] = mapped_column(Text, nullable=True)
+    internet: Mapped[str] = mapped_column(Text, nullable=True)
+    cronos: Mapped[str] = mapped_column(Text, nullable=True)
+    cros: Mapped[str] = mapped_column(Text, nullable=True)
+    addition: Mapped[str] = mapped_column(Text, nullable=True)
+    pfo: Mapped[bool] = mapped_column(Boolean, nullable=True)
+    comments: Mapped[str] = mapped_column(Text, nullable=True)
     conclusion: Mapped[int] = mapped_column(ForeignKey('conclusions.id'))
-    officer: Mapped[str] = mapped_column(String(255))
+    officer: Mapped[str] = mapped_column(String(255), nullable=True)
     deadline: Mapped[datetime] = mapped_column(Date, default=default_time, 
                          onupdate=default_time)
     person_id: Mapped[int] = mapped_column(ForeignKey('persons.id'))
@@ -269,14 +268,14 @@ class Robot(Base):
     __tablename__ = 'robots'
 
     id: Mapped[int] = mapped_column(nullable=False, unique=True, primary_key=True, autoincrement=True)
-    employee: Mapped[str] = mapped_column(Text)
-    inn: Mapped[str] = mapped_column(Text)
-    bankruptcy: Mapped[str] = mapped_column(Text)
-    bki: Mapped[str] = mapped_column(Text)
-    courts: Mapped[str] = mapped_column(Text)
-    terrorist: Mapped[str] = mapped_column(Text)
-    mvd: Mapped[str] = mapped_column(Text)
-    deadline: Mapped[datetime] = mapped_column(Date, default=default_time)
+    employee: Mapped[str] = mapped_column(Text, nullable=True)
+    inn: Mapped[str] = mapped_column(Text, nullable=True)
+    bankruptcy: Mapped[str] = mapped_column(Text, nullable=True)
+    bki: Mapped[str] = mapped_column(Text, nullable=True)
+    courts: Mapped[str] = mapped_column(Text, nullable=True)
+    terrorist: Mapped[str] = mapped_column(Text, nullable=True)
+    mvd: Mapped[str] = mapped_column(Text, nullable=True)
+    deadline: Mapped[datetime] = mapped_column(Date, default=default_time, nullable=True)
     person_id: Mapped[int] = mapped_column(ForeignKey('persons.id'))
     persons: Mapped[List['Person']] = relationship(back_populates='robots')
 
@@ -287,11 +286,11 @@ class Inquiry(Base):
     __tablename__ = 'inquiries'
 
     id: Mapped[int] = mapped_column(nullable=False, unique=True, primary_key=True, autoincrement=True)
-    info: Mapped[str] = mapped_column(Text)
-    initiator: Mapped[str] = mapped_column(String(255))
-    source: Mapped[str] = mapped_column(String(255))
-    officer: Mapped[str] = mapped_column(String(255))
-    deadline: Mapped[datetime] = mapped_column(Date, default=default_time)
+    info: Mapped[str] = mapped_column(Text, nullable=True)
+    initiator: Mapped[str] = mapped_column(String(255), nullable=True)
+    source: Mapped[str] = mapped_column(String(255), nullable=True)
+    officer: Mapped[str] = mapped_column(String(255), nullable=True)
+    deadline: Mapped[datetime] = mapped_column(Date, default=default_time, nullable=True)
     person_id: Mapped[int] = mapped_column(ForeignKey('persons.id'))
     persons: Mapped[List['Person']] = relationship(back_populates='inquiries')
 
@@ -302,12 +301,14 @@ class Connect(Base):
     __tablename__ = 'connects'
 
     id: Mapped[int] = mapped_column(nullable=False, unique=True, primary_key=True, autoincrement=True)
-    company: Mapped[str] = mapped_column(String(255))
-    city: Mapped[str] = mapped_column(String(255))
-    fullname: Mapped[str] = mapped_column(String(255))
-    phone: Mapped[str] = mapped_column(String(255))
-    adding: Mapped[str] = mapped_column(String(255))
-    mobile: Mapped[str] = mapped_column(String(255))
-    mail: Mapped[str] = mapped_column(String(255))
-    comment: Mapped[str] = mapped_column(Text)
-    data: Mapped[datetime] = mapped_column(Date, default=default_time, onupdate=default_time)
+    company: Mapped[str] = mapped_column(String(255), nullable=True)
+    city: Mapped[str] = mapped_column(String(255), nullable=True)
+    fullname: Mapped[str] = mapped_column(String(255), nullable=True)
+    phone: Mapped[str] = mapped_column(String(255), nullable=True)
+    adding: Mapped[str] = mapped_column(String(255), nullable=True)
+    mobile: Mapped[str] = mapped_column(String(255), nullable=True)
+    mail: Mapped[str] = mapped_column(String(255), nullable=True)
+    comment: Mapped[str] = mapped_column(Text, nullable=True)
+    data: Mapped[datetime] = mapped_column(Date, default=default_time, onupdate=default_time, nullable=True)
+
+Base.metadata.create_all(engine)
